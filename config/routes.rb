0ce9e6ac7dfc971818ece0061613b21cce703844
Ubuntu <ubuntu@ip-172-31-16-37.ap-northeast-1.compute.internal>
2025-10-15 09:32:53 +0000

    [Add] create index new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
   get 'books/edit'
-  get '/' => 'homes#top'
+  root to: 'homes#top'
+  get 'books/:id' => 'books#show', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
